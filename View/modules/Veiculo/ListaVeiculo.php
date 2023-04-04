b4281d8b2eb5043eb440d4e1d0944e34122af82d
Joao <hypergamerfrr@gmail.com>
2023-04-04 00:08:45 -0300

    att

--- View/modules/Veiculo/ListaVeiculo.php
+++ View/modules/Veiculo/ListaVeiculo.php
@@ -14,7 +14,6 @@
 
 <table>
   <tr>
-            <th>ID</th>
             <th>Marca</th>
             <th>Modelo</th>
             <th>Fabricante</th>
@@ -22,80 +21,27 @@
             <th>Ano</th>
             <th>Combustivel</th>
             <th>Cor</th>
-            <th>Numero de Chassi</th>
-            <th>Kilometragem</th>
-            <th>Reviso</th>
-            <th>Sinistro</th>
-            <th>Roubo/Furto</th>
-            <th>Aluguel</th>
-            <th>Venda</th>
-            <th>Particular</th>
-            <th>Observação</th>
+            <th>N° Chassi</th>
+            <th>KM</th>
+            <th></th>
+            
+            
+            
   </tr>
   <tr>
   <?php foreach($model->rows as $item): ?>
-    <td> <a href="/veiculo/delete?id=<?= $item->id ?>">Excluir</a></td>
     <td> <?= $item->Marca ?> </td>
     <td> <?= $item->Modelo ?></td>
     <td> <?= $item->Fabricante ?></td>
-    <td> <?= $item->Lista_Tipo ?></td>
+    <td> <?= $item->Tipo ?></td>
     <td> <?= $item->Ano ?></td>
-    <td> <?= $item->Lista_Combustivel ?></td>
+    <td> <?= $item->Combustivel ?></td>
     <td> <?= $item->Cor ?></td>
     <td> <?= $item->NumeroChassi ?></td>
     <td> <?= $item->Kilometragem ?></td>
+    <td> <a href="/veiculo/delete?id=<?= $item->id ?>">erg</a></td>
 
-    <td>
-                <?php if($item->Revisao == '1')
-                {
-                    echo 'Sim';
-                }
-                else if($item->Revisao == '0')
-                    echo 'Não' ?>
-            </td>
-            <td>
-                <?php if($item->Sinistro == '1')
-                {
-                    echo 'Sim';
-                }
-                else if($item->Sinistro == '0')
-                    echo 'Não' ?>
-            </td>
-
-            <td>
-                <?php if($item->Roubo_Furto == '1')
-                {
-                    echo 'Sim';
-                }
-                else if($item->Roubo_Furto == '0')
-                    echo 'Não' ?>
-            </td>
-            
-            <td>
-                <?php if($item->Aluguel == '1')
-                {
-                    echo 'Sim';
-                }
-                else if($item->Aluguel == '0')
-                    echo 'Não' ?>
-            </td>
-            <td>
-                <?php if($item->Venda == '1')
-                {
-                    echo 'Sim';
-                }
-                else if($item->Venda == '0')
-                    echo 'Não' ?>
-            </td>
-            <td>
-            <?php if($item->Particular == '1')
-                {
-                    echo 'Sim';
-                }
-                else if($item->Particular == '0')
-                    echo 'Não' ?>
-            </td>
-            <td><?= $item->Observacoes ?></td> 
+    
 
 
 
